9b20e012df77caf33d7f44593655cd207c737924
Ken Okabe <okbken@gmail.com>
2011-07-03 14:40:09 +0900

    first commit for flextime.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flextime::Application.config.secret_token = '61552179328df75e4fd837ff0bb38983134494b55acff7ae01ed29271a0aff601691f0476b1ef6dff98b851ab70f635e9e552222a93c1ab1cef8c35cc0656e27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Flextime::Application.config.session_store :cookie_store, :key => '_flextime_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Flextime::Application.config.session_store :active_record_store
